475de40ce8099a8a36f3c61ac13d0a6dd00d8c9a
Yun Jehyeok <dbswpgur2@naver.com>
2025-05-28 14:09:16 +0900

    Publishing Mypage Profile

--- src/app/(afterLogin)/mypage/profile/layout.tsx
+++ src/app/(afterLogin)/mypage/profile/layout.tsx
@@ -2,9 +2,15 @@ import LNB from "./_component/LNB";
 
 export default function ProfileLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-[1200px] mx-auto flex">
+        <div>
+            <div className="w-full h-[131px] bg-n200"></div>
+
             <LNB />
-            <div className="flex-1">{children}</div>
+
+            <div className="w-[1200px] flex gap-10 mx-auto relative -top-[50px]">
+                <div className="w-[300px]"></div>
+                <div className="flex-1 flex flex-col gap-4">{children}</div>
+            </div>
         </div>
     );
 }

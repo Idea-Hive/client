729cf90c6bac865bd4cb0920c1073f95ccefceb2
spacewalk00 <ju001217@gmail.com>
2025-05-29 23:02:47 +0900

    프로젝트 관리 - 컴포넌트화 및 피드백 반영

---
+++ src/app/(afterLogin)/mypage/project/_component/RightSection.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+import Button from "@/components/Button";
+import { DownloadSimpleIcon, DownloadSimpleIconWhite } from "@/components/icons/icons";
+import Table from "./Table";
+
+export default function RightSection() {
+    //담당자 드롭박스
+    const [openDropdownIndex, setOpenDropdownIndex] = useState<number | null>(null);
+    const toggleDropdown = (index: number) => {
+        setOpenDropdownIndex((prev) => (prev === index ? null : index));
+    };
+    //탬플릿 다운로드
+    const onDownloadTemplate = () => {
+        //TODO
+    };
+
+    //일단 하드코딩
+    const tasks = [
+        {
+            key: "D_1",
+            title: "[디자인] 와이어프레임",
+            assignee: "홍길동",
+            dueDate: "2025-02-20",
+            submitted: true,
+            isEditable: false,
+        },
+        {
+            key: "D_2",
+            title: "[기획] 회의록 작성",
+            isEditable: true,
+        },
+    ];
+
+    return (
+        <div className="width-[900px] m-10">
+            <div className="text-h2 text-n900 left-0 pb-[16px] border-b-[1px] border-n400">기획</div>
+            <div className="mt-[32px] flex flex-col">
+                <div>
+                    <div className="flex justify-between items-end">
+                        <div className="text-h3 text-n900">필수 과제</div>
+                        <div className="flex gap-2">
+                            <Button
+                                label="선택 탬플릿"
+                                onClick={() => {onDownloadTemplate}}
+                                icLeft={<DownloadSimpleIcon />}
+                                size="small"
+                                btnType="line"
+                            />
+                            <Button
+                                label="전체 탬플릿"
+                                onClick={() => {onDownloadTemplate}}
+                                icLeft={<DownloadSimpleIconWhite />}
+                                size="small"
+                                btnType="primary"
+                            />
+                        </div>
+                    </div>
+                    <Table tasks={tasks} onToggleAssignee={toggleDropdown} openDropdownIndex={openDropdownIndex}></Table>
+                </div>
+                <div className="mt-[40px] flex flex-col">
+                    <div className="text-h3 text-n900">선택 과제</div>
+                    {/* 테이블 */}
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/(afterLogin)/mypage/project/_component/Table.tsx
@@ -0,0 +1,93 @@
+interface Task {
+    key: string;
+    title: string;
+    assignee?: string;
+    dueDate?: string;
+    submitted?: boolean;  //제출 완료 여부
+    isEditable?: boolean; //담당자, 마감기한, 제출 수정 시
+}
+
+interface TaskTableProps {
+    tasks: Task[];
+    onToggleAssignee?: (index: number) => void; //드롭박스 event handler
+    openDropdownIndex?: number | null;          //드롭박스 테이블 idx
+}
+
+import { FolderIcon, SmallUserImgIcon, CaretDownIcon, CalendaBlankIcon, UploadSimpleIcon
+} from "@/components/icons/icons";
+
+const Table: React.FC<TaskTableProps> = ({ tasks, onToggleAssignee, openDropdownIndex }) => {
+    return (
+        <div className="mt-4 rounded border border-n400">
+            <table className="w-full text-xs border-separate border-spacing-0">
+                <thead className="bg-n50 text-center text-xsEmphasis">
+                    <tr className="h-[42px]">
+                        <th className="border-b pt-3 pb-3 rounded-tl-xl w-[64px]">선택</th>
+                        <th className="border-b pt-3 pb-3 w-[74px]">Key</th>
+                        <th className="border-b pt-3 pb-3 w-[243px]">과제</th>
+                        <th className="border-b pt-3 pb-3 w-[120px]">담당자</th>
+                        <th className="border-b pt-3 pb-3 w-[140px]">마감기한</th>
+                        <th className="border-b pt-3 pb-3 rounded-tr-xl w-[158px]">제출</th>
+                    </tr>
+                </thead>
+                <tbody className="bg-n0 text-sm">
+                    {tasks.map((task, index) => (
+                        <tr key={task.key} className={`border-t h-[42px] ${index === tasks.length - 1 ? "last-row" : ""}`}>
+                            <td className={`p-3 text-center ${index === tasks.length - 1 ? "rounded-bl-xl" : "border-b"}`}>
+                                <input type="checkbox" className="w-4 h-4 border-n400"/>
+                            </td>
+                            <td className={`p-3 border-l ${index === tasks.length - 1 ? "" : "border-b"}`}>{task.key}</td>
+                            <td className={`p-3 border-l ${index === tasks.length - 1 ? "" : "border-b"}`}>{task.title}</td>
+                            <td className={`relative p-3 border-l text-center ${index === tasks.length - 1 ? "" : "border-b"}`}>
+                                {task.isEditable ? (
+                                    <div className="flex justify-center items-center gap-1 text-n600 cursor-pointer" onClick={() => onToggleAssignee?.(index)}>
+                                        <span>담당자 선택</span>
+                                        <CaretDownIcon />
+                                    </div>
+                                ) : (
+                                    <div className="flex justify-center items-center gap-1">
+                                        <SmallUserImgIcon />
+                                        <span>{task.assignee}</span>
+                                    </div>
+                                )}
+                                {openDropdownIndex === index && (
+                                    <div className="absolute right-0 z-10">
+                                        <ul className="w-[120px] bg-white border rounded shadow text-left">
+                                            <li className="h-[36px] px-3 py-2 hover:bg-n200">이서연</li>
+                                            <li className="h-[36px] px-3 py-2 hover:bg-n200">홍길동</li>
+                                        </ul>
+                                    </div>
+                                )}
+                            </td>
+                            <td className={`p-3 text-center border-l ${index === tasks.length - 1 ? "" : "border-b"}`}>
+                                {task.isEditable ? (
+                                    <div className="flex justify-center items-center gap-1 text-n600 cursor-pointer">
+                                        <CalendaBlankIcon />
+                                        <span>기한 설정</span>
+                                    </div>
+                                ) : (
+                                    task.dueDate
+                                )}
+                            </td>
+                            <td className={`p-3 text-center border-l ${index === tasks.length - 1 ? "rounded-br-xl" : "border-b"}`}>
+                                {task.isEditable ? (
+                                    <div className="flex justify-center items-center gap-1 text-n600 cursor-pointer">
+                                        <UploadSimpleIcon />
+                                        <span>제출하기</span>
+                                    </div>
+                                ) : (
+                                    <div className="flex justify-center items-center gap-1">
+                                        <FolderIcon />
+                                        <span>{task.submitted ? "제출완료" : "미제출???"}</span>
+                                    </div>
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Table;
